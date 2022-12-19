ddd024b298770680447646958c5f0cefc2ed8330
Omar-Codess <omartaki8@gmail.com>
2022-12-19 20:09:04 +0100

    creato app.blade.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [HomeController::class, 'show'])->name('movies.show');
